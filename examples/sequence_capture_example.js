e22f9eeb1985ae213499e6ac0d0f4a9a549dbedd
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-06-15 23:58:52 +0530

    update sequence capture example

--- examples/sequence_capture_example.js
+++ examples/sequence_capture_example.js
@@ -14,25 +14,20 @@ console.log('Actual properties',
     '\nCx', sequenceCapture.getCx(),
     '\nCy', sequenceCapture.getCy(),
 );
-// let img = sequenceCapture.getNextFrame();
-// console.log(`nextSequence is Uint8Array: ${img instanceof Uint8Array} | size: ${img.length}`);
-//
-// let grayImg = sequenceCapture.getGrayFrame();
-// console.log(`grayFrame is Uint8Array: ${grayImg instanceof Uint8Array}| size: ${grayImg.length}`);
-//
-// console.log('New properties',
-//     '\nprogress', sequenceCapture.getProgress(),
-//     '\nSequence Ht', sequenceCapture.getFrameHeight(),
-//     '\nSequence Wd', sequenceCapture.getFrameWidth(),
-//     '\nFx', sequenceCapture.getFx(),
-//     '\nFy', sequenceCapture.getFy(),
-//     '\nCx', sequenceCapture.getCx(),
-//     '\nCy', sequenceCapture.getCy(),
-// );
-// sequenceCapture.getNextFrame();
-//
-// let bboxes = sequenceCapture.getBoundingBoxes();
-// console.log(`bboxes is Array: ${Array.isArray(bboxes)}| size: ${bboxes.length}`);
-// for(let rect of bboxes) {
-//     console.log(`x:${rect.x}, y:${rect.y}, w:${rect.width}, h:${rect.height}`);
-// }
+let img = sequenceCapture.getNextFrame();
+console.log(`nextSequence is Uint8Array: ${img instanceof Uint8Array} | size: ${img.length}`);
+
+let grayImg = sequenceCapture.getGrayFrame();
+console.log(`grayFrame is Uint8Array: ${grayImg instanceof Uint8Array}| size: ${grayImg.length}`);
+
+console.log('New properties',
+    '\nprogress', sequenceCapture.getProgress(),
+    '\nSequence Ht', sequenceCapture.getFrameHeight(),
+    '\nSequence Wd', sequenceCapture.getFrameWidth(),
+    '\nFrame number', sequenceCapture.getFrameNumber(),
+    '\nFx', sequenceCapture.getFx(),
+    '\nFy', sequenceCapture.getFy(),
+    '\nCx', sequenceCapture.getCx(),
+    '\nCy', sequenceCapture.getCy(),
+);
+sequenceCapture.getNextFrame();
