1737f38bce74c9473d67993675ca1edee28227db
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-06-21 18:03:39 +0530

    update mtccn example

--- examples/mtcnn_on_video.js
+++ examples/mtcnn_on_video.js
@@ -8,13 +8,14 @@ sequenceCapture.openVideoFile(argv[0])
 mtcnn.read(argv[1]);
 
 img = sequenceCapture.getNextFrame();
+console.log(`Frame: rows:${img.height()}, cols:${img.width()}, channels:${img.channels()}`);
 let res = mtcnn.detectFaces(img);
 
 for(let i = 2; i < 60; i++) {
-    console.log(`res frame[${i}]: ${res}`);
+    console.log(`res frame[${i}]- bboxes: ${res.detections[0].x},${res.detections[0].y},${res.detections[0].width},${res.detections[0].height} | conf: ${res.confidences[0]}`);
     nodoface.showImage(img, 'output');
-    nodoface.waitKey(0);
+    nodoface.waitKey(1000/25);
     img = sequenceCapture.getNextFrame();
-    console.log(`Frame[${i}]: rows:${img.height()}, cols:${img.width()}, channels:${img.channels()}`);
+    res = mtcnn.detectFaces(img);
 }
 nodoface.destroyAllWindows();
