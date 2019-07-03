ef1cae5ff702a8ad7135e3c593285ef683e8105a
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-07-03 12:03:57 +0530

    Inherit from MatImage

--- src/cvtypes/MatFloat.h
+++ src/cvtypes/MatFloat.h
@@ -13,36 +13,14 @@
 
 #include "../extras/napiextra.h"
 #include "../jserrors/JSErrors.h"
+#include "MatImage.h"
 
 namespace Nodoface {
     // Wraps OpenCV Mat_<float> partially for sending and receiving Mat_<float> objects to/from node
-    class FloatImage : public Napi::ObjectWrap<FloatImage> {
+    class FloatImage : public Nodoface::MatImage<float> {
 
     public:
         static Napi::Object Init(Napi::Env env, Napi::Object exports);
-
-        static Napi::Value NewObject(Napi::Env env, cv::Mat_<float>& mat);
-
-        static cv::Mat_<float>* NewMat(Napi::TypedArrayOf<float>& arr, Napi::Number& rows, Napi::Number& cols, Napi::Number& type);
-
-        FloatImage(const Napi::CallbackInfo& info);
-
-        cv::Mat_<float> GetMat();
-
-        ~FloatImage();
-    private:
-
-        cv::Mat_<float>* mat;
-
-        static Napi::FunctionReference constructor;
-
-        Napi::Value Type(const Napi::CallbackInfo& info);
-
-        Napi::Value Rows(const Napi::CallbackInfo& info);
-
-        Napi::Value Columns(const Napi::CallbackInfo& info);
-
-        Napi::Value Channels(const Napi::CallbackInfo& info);
     };
 }
 
