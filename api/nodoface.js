f24cc77059a30c958e7a0f63fc12cf6ccea58c83
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-08-03 15:05:11 +0530

    npm publish

--- api/nodoface.js
+++ api/nodoface.js
@@ -3,7 +3,7 @@ let nodoface = null;
 if (process.env.NODOFACE_BUILD_TYPE_DEBUG) {
     nodoface = require(path.resolve(path.join(__dirname, '..', 'build', 'Debug', 'nodoface.node')));
 } else {
-    console.log(path.resolve(path.join(__dirname, '..', 'build', 'Release', 'nodoface.node')));
+    // console.log(path.resolve(path.join(__dirname, '..', 'build', 'Release', 'nodoface.node')));
     nodoface = require(path.resolve(path.join(__dirname, '..', 'build', 'Release', 'nodoface.node')));
 }
 
